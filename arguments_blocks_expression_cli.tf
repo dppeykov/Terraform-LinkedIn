22efcb97de6ba4f21879f680a3b78c7fc6a8e0f6
Damyan Peykov <dppeykov@gmail.com>
2019-11-08 09:42:50 +0100

    DOCS CLI

---
+++ arguments_blocks_expression_cli.tf
@@ -0,0 +1,10 @@
+# https://www.terraform.io/docs/configuration/index.html
+
+resource "aws_vpc" "main" {
+  cidr_block = var.base_cidr_block
+}
+
+<BLOCK TYPE> "<BLOCK LABEL>" "<BLOCK LABEL>" {
+  # Block body
+  <IDENTIFIER> = <EXPRESSION> # Argument
+}
